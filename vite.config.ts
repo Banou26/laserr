c1666ab1d483a1abe86cb341f9c499135297822e
Banou <me@banou.dev>
2023-04-02 05:17:44 +0200

    feat: ✨setup vite build config

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import typescript2 from "rollup-plugin-typescript2"
+
+import pkg from './package.json'
+
+export default defineConfig({
+  build: {
+    target: 'esnext',
+    lib: {
+      formats: ['es'],
+      entry: './src/index.ts',
+      name: 'index',
+      fileName: 'index'
+    },
+    minify: false,
+    sourcemap: true,
+    outDir: 'build',
+    rollupOptions: {
+      external: [
+        ...Object
+          .keys(pkg.dependencies),
+        'scannarr'
+      ]
+    }
+  },
+  plugins: [
+    {
+      ...typescript2({
+        abortOnError: false
+      }),
+      apply: 'build'
+    }
+  ]
+})
