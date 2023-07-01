50bccf208f3439fd65c7a534987f108e0d98782d
Banou <me@banou.dev>
2023-07-01 11:53:52 +0200

    feat: ✨add anidb as empty source

--- src/targets/index.ts
+++ src/targets/index.ts
@@ -3,6 +3,7 @@ import type { Target } from 'scannarr'
 import * as MAL from './myanimelist'
 import * as Nyaasi from './nyaasi'
 import * as Anilist from './anilist'
+import * as AniDB from './anidb'
 import * as Crunchyroll from './crunchyroll'
 import * as AnimeTosho from './animetosho'
 
@@ -10,6 +11,7 @@ export const targets: Target[] = [
   MAL,
   Nyaasi,
   Anilist,
+  AniDB,
   Crunchyroll,
   AnimeTosho
 ]
